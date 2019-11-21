ded506df6ae694945c24382b8b9120b597038e2f
ClebesonMouraiffar <clebeson.moura@aluno.iffar.edu.br>
2019-11-20 23:05:30 -0300

    Profile Page OK

--- web/src/components/Headers/ProfilePageHeader.js
+++ web/src/components/Headers/ProfilePageHeader.js
@@ -38,22 +38,8 @@ function ProfilePageHeader() {
           <div className="photo-container">
             <img alt="..." src={require("assets/img/vivelavie.png")}></img>
           </div>
-          <h3 className="title">Ryan Scheinder</h3>
-          <p className="category">Photographer</p>
-          <div className="content">
-            <div className="social-description">
-              <h2>26</h2>
-              <p>Comments</p>
-            </div>
-            <div className="social-description">
-              <h2>26</h2>
-              <p>Comments</p>
-            </div>
-            <div className="social-description">
-              <h2>48</h2>
-              <p>Bookmarks</p>
-            </div>
-          </div>
+          <h3 className="title">Vive La Vie</h3>
+          <p className="category">Club</p>          
         </Container>
       </div>
     </>
