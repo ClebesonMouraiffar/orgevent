cbb062d08bf9fda68f7a6059354b1eb82ace0bed
ClebesonMouraiffar <clebeson.moura@aluno.iffar.edu.br>
2019-11-20 22:36:39 -0300

    Home ok

--- web/src/views/index-sections/EventFeatured.js
+++ web/src/views/index-sections/EventFeatured.js
@@ -1,79 +1,92 @@
-    import React from "react";
+import React from "react";
 
-    // reactstrap components
-    import { Button, Container, Row, Col } from "reactstrap";
+// reactstrap components
+import { Button, Container, Row, Col } from "reactstrap";
 
-    // core components
+// core components
 
-    function BestEvent() {
+function BestEvent() {
     return (
         <>
-        <div className="section">
-            <Container>
-            <h3 className="title">Eventos em Destaque</h3>          
-            <div id="images">
-                <Row>
-                <Col sm="3" href="/#">
-                <Button
-                    outline
-                    color="default"
-                    href="/landing-page"
-                >                    
-                    <img
-                    alt="..."
-                    className="rounded img-raised mr-1"
-                    src={require("assets/img/julie.jpg")}                  
-                    ></img>
-                    <p className="category">Circle Raised</p>
-                </Button>                
-                </Col><Col sm="3" href="/#">
-                <Button
-                    outline
-                    color="default"
-                    href="/landing-page"
-                >                    
-                    <img
-                    alt="..."
-                    className="rounded img-raised mr-1"
-                    src={require("assets/img/julie.jpg")}                  
-                    ></img>
-                    <p className="category">Circle Raised</p>
-                </Button>                
-                </Col>
-                <Col sm="3" href="/#">
-                <Button
-                    outline
-                    color="default"
-                    href="/landing-page"
-                >                    
-                    <img
-                    alt="..."
-                    className="rounded img-raised mr-1"
-                    src={require("assets/img/julie.jpg")}                  
-                    ></img>
-                    <p className="category">Circle Raised</p>
-                </Button>                
-                </Col>
-                <Col sm="3" href="/#">
-                <Button
-                    outline
-                    color="default"
-                    href="/landing-page"
-                >                    
-                    <img
-                    alt="..."
-                    className="rounded img-raised mr-1"
-                    src={require("assets/img/julie.jpg")}                  
-                    ></img>
-                    <p className="category">Circle Raised</p>
-                </Button>                
-                </Col>
-                </Row>
+            <div className="section">
+                <Container>
+                    <h3 className="title">Eventos em Destaque</h3>
+                    <div id="images">
+                        <Row>
+                            <Col sm="3" href="/#">
+                                <Button
+                                    outline
+                                    color="default"
+                                    href="/landing-page"
+                                >
+                                    <img
+                                        alt="..."
+                                        className="rounded img-raised mr-1"
+                                        src={require("assets/img/lumi.jpg")}
+                                    ></img>
+                                    <p className="category">LUMI</p>
+                                    <i className="now-ui-icons ui-1_calendar-60"></i>
+                                    <p>De: 23 Nov às 00:00</p>
+                                    <p>Até: 24 Nov às 06:00</p>
+                                </Button>
+                            </Col>
+                            <Col sm="3" href="/#">
+                                <Button
+                                    outline
+                                    color="default"
+                                    href="/landing-page"
+                                >
+                                    <img
+                                        alt="..."
+                                        className="rounded img-raised mr-1"
+                                        src={require("assets/img/lumi.jpg")}
+                                    ></img>
+                                    <p className="category">LUMI</p>
+                                    <i className="now-ui-icons ui-1_calendar-60"></i>
+                                    <p>De: 23 Nov às 00:00</p>
+                                    <p>Até: 24 Nov às 06:00</p>
+                                </Button>
+                            </Col>
+                            <Col sm="3" href="/#">
+                                <Button
+                                    outline
+                                    color="default"
+                                    href="/landing-page"
+                                >
+                                    <img
+                                        alt="..."
+                                        className="rounded img-raised mr-1"
+                                        src={require("assets/img/lumi.jpg")}
+                                    ></img>
+                                    <p className="category">LUMI</p>
+                                    <i className="now-ui-icons ui-1_calendar-60"></i>
+                                    <p>De: 23 Nov às 00:00</p>
+                                    <p>Até: 24 Nov às 06:00</p>
+                                </Button>
+                            </Col>
+                            <Col sm="3" href="/#">
+                                <Button
+                                    outline
+                                    color="default"
+                                    href="/landing-page"
+                                >
+                                    <img
+                                        alt="..."
+                                        className="rounded img-raised mr-1"
+                                        src={require("assets/img/lumi.jpg")}
+                                    ></img>
+                                    <p className="category">LUMI</p>
+                                    <i className="now-ui-icons ui-1_calendar-60"></i>
+                                    <p>De: 23 Nov às 00:00</p>
+                                    <p>Até: 24 Nov às 06:00</p>
+                                </Button>
+                            </Col>
+                        </Row>
+                    </div>
+                </Container>
             </div>
-            </Container>
-        </div>
         </>
     );
-    }
+}
 
-    export default BestEvent;
+export default BestEvent;
--- web/src/views/index-sections/HomeFeatured.js
+++ web/src/views/index-sections/HomeFeatured.js
@@ -10,7 +10,7 @@ function BestEvent() {
     <>
       <div className="section">
         <Container>
-          <h3 className="title">Casas em destaque</h3>          
+          <h3 className="title">Casas em Destaque</h3>          
           <div id="images">
             <Row>
               <Col sm="2">
@@ -18,11 +18,76 @@ function BestEvent() {
                 color="link"
                 href="/profile-page"
               >
-                <p className="category">Circle Raised</p>
+                <p className="category">Vive La Vie</p>
                 <img
                   alt="..."
                   className="rounded-circle img-raised mr-1"
-                  src={require("assets/img/julie.jpg")}                  
+                  src={require("assets/img/vivelavie.png")}                  
+                ></img>
+              </Button>                
+              </Col>
+              <Col sm="2">
+              <Button
+                color="link"
+                href="/profile-page"
+              >
+                <p className="category">JhonGarden</p>
+                <img
+                  alt="..."
+                  className="rounded-circle img-raised mr-1"
+                  src={require("assets/img/jhongarden.jpg")}                  
+                ></img>
+              </Button>                
+              </Col>
+              <Col sm="2">
+              <Button
+                color="link"
+                href="/profile-page"
+              >
+                <p className="category">MTM Eventos</p>
+                <img
+                  alt="..."
+                  className="rounded-circle img-raised mr-1"
+                  src={require("assets/img/mtmeventos.jpg")}                  
+                ></img>
+              </Button>                
+              </Col>
+              <Col sm="2">
+              <Button
+                color="link"
+                href="/profile-page"
+              >
+                <p className="category">Rudah</p>
+                <img
+                  alt="..."
+                  className="rounded-circle img-raised mr-1"
+                  src={require("assets/img/rudah.jpg")}                  
+                ></img>
+              </Button>                
+              </Col>
+              <Col sm="2">
+              <Button
+                color="link"
+                href="/profile-page"
+              >
+                <p className="category">Imperial Eventos</p>
+                <img
+                  alt="..."
+                  className="rounded-circle img-raised mr-1"
+                  src={require("assets/img/imperial.jpg")}                  
+                ></img>
+              </Button>                
+              </Col>
+              <Col sm="2">
+              <Button
+                color="link"
+                href="/profile-page"
+              >
+                <p className="category">Havana</p>
+                <img
+                  alt="..."
+                  className="rounded-circle img-raised mr-1"
+                  src={require("assets/img/havana.jpg")}                  
                 ></img>
               </Button>                
               </Col>
